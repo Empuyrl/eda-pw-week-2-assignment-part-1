0807f6d98b4bc0450e55fee9932bff199e926a4a
User <user@Users-MacBook-Pro.local>
2023-04-03 15:59:49 -0500

    project done

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,44 +1,80 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
+var  firstName =  Jack;
 
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
+var lastName =  Blonigen;
 
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
+var fullName = 'firstName' + 'lastName'
 
 // 4 - Console log the value of `fullName`
+console.log("fullName");
 
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
+var luckyNumber = 7;
 
 // 6 - Console log this sentence, adding in the variables you created above: 
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 // Refer back to the videos if you need help with this one.
+console.log('My name is:' fullName, 'and I think:' luckyNumber 'is a winner!')
+
 
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
-
+var adventurous
+var Yes = true;
+var No = false;
+if(Yes){
+    alert("is adventurous")
+}
+if(No){
+    alert("is not adventurous")
+}
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
-
+var food  = Ramen;
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
-
+var pets = One;
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
+var friendsPets = Two;
 
 // 11 - Add two pets to your `pets` variable
+var pets = One;
+var morePets = Two;
+pets + morePets = Three;
 
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
-
+const allowedPets = Four;
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
-
+if (adventurous === true) {
+    console.log("Adventures are great!")
+} else if (adventurous === false) {
+    console.log("How about we stay home")
+}
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
-
+var luckyNumber = 2;
+if (luckyNumber = 2 & adventurous === true)
+console.log("Roll the dice!")
 // 15 - Write a conditional that console logs "I can have more pets!" 
 //if the value of `pets` is less than the value of `allowedPets`,
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
 // and console logs "Oh no, I have too many pets!"
 // if the value of `pets` is greater than the value of `allowedPets`.
-
+if (pets < allowedPets)
+{
+    console.log("I can have more pets!")
+}
+if (pets === allowedPets)
+{
+    console.log("I have enough pets")
+}
+if (pets > allowedPets)
+{
+    console.log("Oh no, I have too many pets!")
+}
 // STRETCH GOALS:
 
 // 16 - Make a variable called `mostPets` and a conditional that
@@ -53,7 +89,44 @@
 //      "Third is the one with the polka dot dress" if your lucky number is 3
 //      Otherwise, log "Luck is what happens when preparation meets opportunity"
 //      You'll need to research how to use switch statements!
+switch(luckyNumber)
+case 0:
+    luckyNumber = 1
+    break;
+   
+case 1:
+    luckyNumber = 2
+    break;
+
+case 2:
+    luckyNumber = 3
+    break;
+
+    if(luckyNumber = 1)
+    {
+        console.log("First is the worst")
+    }
+
+    if(luckyNumber = 2)
+    {
+        console.log("Second is the best")
+    }
+
+    if(luckyNumber = 3)
+    {
+        console.log("Third is the one with the polka dot dress")
+    }
+
+    console.log("Luck is what happens when preparation meets opportunity")
 
 // 18 -- Rewrite question 13 with a `ternary` operator. You'll need to do research!
+let person = adventurous;
+let result;
 
+if (true) {
+    result = "Adventures are great!"
+} else {
+    result = "How about we stay home?"
+}
 
+console.log(result);
